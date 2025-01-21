77def4d35b484c00428ffc836bd105038a1b5a25
iKazai <nba.kmob92@gmail.com>
2025-01-21 12:00:04 +0100

    initialize project structure with basic configuration and application files

---
+++ Backend/src/main/java/com/eco/e_co/ECommerApplication.java
@@ -0,0 +1,13 @@
+package com.eco.e_co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/eco/e_co/ECommerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eco.e_co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
